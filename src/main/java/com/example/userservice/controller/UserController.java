b676b00d54c156cf75bec977fc1363dc7d75c814
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2022-11-22 11:10:29 +0700

    Initial commit

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public String getUser(@RequestHeader(value = "id") String userId, @RequestHeader(value = "role") String role) {
+        return "User (ID: " + userId + ", role: " + role + ")";
+    }
+}
